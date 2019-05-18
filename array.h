b6319d583d75a6f4cf5b560fd7bc9f0c0fccc658
Groysman881 <44917666+Groysman881@users.noreply.github.com>
2019-05-18 21:27:51 +0300

    Add files via upload

--- array.h
+++ array.h
@@ -1,24 +1,33 @@
 #ifndef ARRAY_H
 #define ARRAY_H
+#include<iostream>
+
+/*template<typename T>
+class Iterator{
+private:
+    T* mas;
+public:
+    Iterator(){mas = nullptr;}
+    Iterator(T* arr): mas(arr){};
+    T operator*(){return (*mas);}
+    Iterator<T>& operator++(){mas++;return *this;}
+    bool operator!=(const Iterator<T> &iter1){return mas != iter1.mas;}
+};*/
 
 template<typename T>
 class Array
 {
 public:
-    T* ptr;
+    class Iterator;
+    T* ptr;   
     size_t size;
     size_t capacity;
 
-    class Iterator
-    {
-
-    };
-
     Array(){
         std::cout<<"Check"<<std::endl;
-        ptr = new T [1];
-        size = 1;
-        capacity = 1;
+        ptr = nullptr;
+        size = 0;
+        capacity = 0;
     }
     ~Array(){
         delete(ptr);
@@ -31,14 +40,14 @@ public:
         }
         else{
             for(int i = 0;i < size;i++){
-                new(ptr + i) T *(arr.ptr + i);
+                ptr = new (arr.ptr) T [size];
             }
             size = arr.size;
             capacity = arr.capacity;
         }
     }
 
-    T operator[](size_t index){
+    T& operator[](size_t index){
         try
         {
             if(index < size){
@@ -52,31 +61,79 @@ public:
             exit(0);
         }
     }
-    void operator=(const Array<T> &arr){
 
+    void insert(T element,size_t pos){
+        if(pos <= size){
+            if(size == capacity){
+                capacity<<1;
+                T* buf = new T [capacity];
+                for (int i = 0;i < pos;i++){
+                    new (buf + i) T (*(ptr + i));
+                }
+                new (buf + pos - 1) T (element);
+                for(int i = pos;i < size;i++){
+                    new (buf + i) T (*(ptr + pos - 1))  ;
+                }
+                delete(ptr);
+                ptr = buf;
+            }
+            else if(size < capacity){
+                for(int i = pos;i < size;i++){
+                    new (ptr + pos + i) T (*(ptr + pos + i - 1));
+                }
+                new (ptr + pos - 1) T (element);
+            }
+        }
+        else{
+            std::cout<<"Out of Bounds!"<<std::endl;
+            exit(0);
+        }
+        size++;
     }
-    void insert(T element);
-    void erase(T element);
+    void erase(size_t pos){
+        if(pos <= size){
+
+        }
+    }
     void push_back(T element){
         if(size == capacity){
             if(size == 0){
-                ptr = new T(1);
+                ptr = new T[1];
                 std::cout<<"Check"<<std::endl;
                 capacity = 1;
             }
             else{
                 capacity = 2 * capacity;
-                T* buf = new T(capacity);
+                T* buf = new T[capacity];
                 for(int i = 0;i < size;i++){
-                    new(buf + i) T *(ptr + i);
+                    new (buf + i) T (*(ptr + i));
                 }
+                std::cout<<buf[0]<<std::endl;
                 delete(ptr);
                 ptr = buf;
+                std::cout<<ptr[0]<<std::endl;
             }
         }
         size++;
         *(ptr + size - 1) = element;
     }
+    size_t getSize(){return size;}
+    Iterator begin(){return ptr;}
+    Iterator end(){return ptr + size;}
+
+    class Iterator{
+    private:
+        T* mas;
+    public:
+        Iterator(T* _begin): mas(_begin){};
+        T& operator*(){return (*mas);}
+        Iterator& operator++(){mas++;return *this;}
+        Iterator& operator--(){mas--;return *this;}
+        bool operator!=(const Iterator &iter1){return mas != iter1.mas;}
+    };
+
 };
+
+
 
 #endif // ARRAY_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include"array.h"
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     Array<int> arr;
-    //arr.push_back(12);
+    arr.push_back(12);
+    arr.push_back(14);
+    arr.push_back(15);
+    arr.insert(10,1);
     Array<int> arr1(arr);
+    cout<<"Check"<<arr[0]<<endl;
     return 0;
 }
