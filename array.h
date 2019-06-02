ce8a6f9189558c2902bc5c403f2529d93fa52e6a
Groysman881 <44917666+Groysman881@users.noreply.github.com>
2019-06-02 13:20:13 +0300

    Add files via upload

--- array.h
+++ array.h
@@ -8,9 +8,9 @@ template<typename T>
 class Array
 {
 private:
-	T* ptr;
-	size_t size;
-	size_t capacity;	
+        T* ptr;
+        size_t size;
+        size_t capacity;
 public:
     struct Iterator{
         T* mas;
@@ -64,30 +64,33 @@ public:
     }
 
     void insert(T element,size_t pos){
-	        try
+        try
         {
             if(pos < size){
                 if(size == capacity){
-                capacity*=2;
-                char* buf = new char [sizeof(T) * capacity];
-                T* buf1 = reinterpret_cast<T*>(buf);
-                for (size_t i = 0;i < pos - 1;i++){
-                    new (buf1 + i) T (*(ptr + i));
-                }
-                new (buf1 + pos) T (element);
-                for(size_t i = pos + 1;i < size;i++){
-                        new (buf1 + i) T (*(ptr + pos - 1));
-                }
-                delete(ptr);
-                ptr = buf1;
+                    capacity*=2;
+                    char* buf = new char [sizeof(T) * capacity];
+                    T* buf1 = reinterpret_cast<T*>(buf);
+                    for (size_t i = 0;i < pos;i++){
+                        new (buf1 + i) T (*(ptr + i));
+                    }
+                    new (buf1 + pos) T (element);
+                    for(size_t i = pos + 1;i <= size;i++){
+                        new (buf1 + i) T (*(ptr + i - 1));
+                    }
+                    for(int i = 0;i < size;i++){
+                        (ptr + i)->~T();
+                    }
+                    delete(ptr);
+                    ptr = buf1;
                 }
                 else{
                     for(int i = size;i >= pos + 1;i--){
                         new (ptr + i) T (*(ptr + i - 1));
                         (ptr + i - 1)->~T();
                     }
+                    new (ptr + pos) T (element);
                 }
-                new (ptr + pos) T (element);
             }
             else{
                 throw std::out_of_range("Out of bounds");
@@ -95,8 +98,8 @@ public:
             size++;
         }
         catch(std::exception e){
-                std::cout<<"Out of bounds"<<std::endl;
-                exit(0);
+            std::cout<<"Out of bounds"<<std::endl;
+            exit(0);
         }
     }
     void insert(T element,Iterator pos){
@@ -106,7 +109,7 @@ public:
             ++pos_finder;
             ++idx;
         }
-            insert(element,idx);
+        insert(element,idx);
     }
     void erase(size_t pos){
         try{
@@ -114,22 +117,22 @@ public:
                 for(size_t i = pos;i < size - 1;i++){
                     new (ptr + i) T (*(ptr + i + 1));
                     (ptr + i + 1)->~T();
-            	}	
-            	while(size < capacity/2){
+                }
+                    size--;
+                while(size <= capacity/2){
                     capacity/=2;
                     char* buf = new char [sizeof(T) * capacity];
                     T* buf1 = reinterpret_cast<T*>(buf);
                     for(size_t i = 0;i < size;i++){
-                    	new (buf1 + i) T (*(ptr + i));
+                        new (buf1 + i) T (*(ptr + i));
                     }
-                    for(int i = 0;i < size;i++){
+                    for(size_t i = 0;i < size;i++){
                         (ptr + i)->~T();
-	                delete(ptr);
-                        ptr = buf1;
                     }
-                    size--;
-                }
-	    }
+                    delete(ptr);
+                    ptr = buf1;
+                    }
+            }
             else{
                 throw std::out_of_range("Out of bounds");
             }
@@ -154,7 +157,7 @@ public:
                     new (buf1 + i) T (*(ptr + i));
                 }
                 for(int i = 0;i < size;i++){
-                        (ptr + i)->~T();
+                    (ptr + i)->~T();
                 }
                 delete(ptr);
                 ptr = buf1;
--- main.cpp
+++ main.cpp
@@ -4,26 +4,57 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+	bool cont = 1;
+	int a;
     Array<int> arr;
-    arr.push_back(12);
-    arr.push_back(14);
-    arr.push_back(15);
-    arr.insert(10,0);
-    arr.push_back(156);
-    arr.print();
-    Array<int> arr1(arr);
-    arr1.print();
-    cout<<"Check"<<arr[0]<<endl;
-    arr1.erase(0);
-    arr1.erase(0);
-    arr1.erase(0);
-    cout<<"PRINT"<<endl;
-    arr1.print();
-    cout<<"Check"<<arr1[0]<<endl;
-    cout<<"CHECK"<<endl;
-    Array<int>::Iterator iter1(arr.begin());
-    ++iter1;
-    cout<<*iter1<<endl;
-    arr.print();
-    return 0;
+    while(cont){
+		cout<<"1) Push back"<<endl;
+		cout<<"2) Insert"<<endl;
+		cout<<"3) Erase"<<endl;
+		cout<<"4) Print"<<endl;
+        cout<<"5) Exit"<<endl;
+		cout<<"Enter action : ";
+		cin>>a;
+		switch(a){
+			case 1:
+			{
+				cout<<"Value : ";
+				int val;
+				cin>>val;
+				arr.push_back(val);
+                break;
+			}
+			case 2:
+			{
+				cout<<"Pos : ";
+				size_t pos;
+				cin>>pos;
+				int val;
+				cout<<"Value : ";
+				cin>>val;
+				arr.insert(val,pos);
+                break;
+			}
+			case 3:
+			{
+				cout<<"Pos : ";
+				size_t pos;
+				cin>>pos;
+                arr.erase(pos);
+                break;
+			}
+			case 4:
+            {
+				arr.print();
+                break;
+            }
+			case 5:
+            {
+                cont = 0;
+                break;
+            }
+		}
+	}
+	return (0);
+	   
 }
